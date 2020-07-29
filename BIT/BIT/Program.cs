53432f609f566ece43d3d227047df3b3e350d6b1
dczzzzzdc <67943970+dczzzzzdc@users.noreply.github.com>
2020-07-29 21:17:16 +0800

    Adding Basic Bitwise operator

--- BIT/BIT/Program.cs
+++ BIT/BIT/Program.cs
@@ -8,21 +8,33 @@ namespace BIT
         {
             /* Bitwise operator
              *  And & (Both)
-             *  Only save the bit that are the same in two numbers
+             *  If both two bits are 1, the result is 1
              *  
              *  Or | (Either)
-             *  If any of the bit is 1, then the result is one
+             *  If any of the bit is 1, then the result is 1
              *  
              *  Xor ^ (Different)
+             *  If only one of the bits is 1, the result is 1
+             * 
              *  Not ~ (Invert)
+             *  Revert all the bits
              */
-            int a = 25;
-            int b = 7;
+            byte a = 25;
+            byte b = 7;
             Console.WriteLine("a: " + Convert.ToString(a, 2).PadLeft(8,'0'));
             Console.WriteLine("b: " + Convert.ToString(b, 2).PadLeft(8,'0'));
             Console.WriteLine("--------");
             Console.WriteLine("And Operation");
             Console.WriteLine(Convert.ToString(a & b, 2).PadLeft(8,'0'));
+            Console.WriteLine();
+            Console.WriteLine("Or Operation");
+            Console.WriteLine(Convert.ToString(a|b,2).PadLeft(8,'0'));
+            Console.WriteLine();
+            Console.WriteLine("Xor Operation");
+            Console.WriteLine(Convert.ToString(a ^ b,2).PadLeft(8,'0'));
+            Console.WriteLine();
+            Console.WriteLine("A Not Operation");
+            Console.WriteLine(Convert.ToString((byte)~a,2));
         }
     }
 }
