575248c674e2a275501cd601ed2ffbf23380018d
dczzzzzdc <67943970+dczzzzzdc@users.noreply.github.com>
2020-08-08 16:47:40 +0800

    Adding Leetcode 12

--- Math/Math/Program.cs
+++ Math/Math/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Math
 {
@@ -24,6 +25,26 @@ namespace Math
                 x /= 10;
             }
             return ans;
+        }
+        #endregion
+        #region Leetcode 12  Integer to Roman
+        public string IntToRoman(int num)
+        {
+            StringBuilder res = new StringBuilder("");
+            string[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] value = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            int index = 0;
+            while (num > 0)
+            {
+                int count = num / value[index];
+                for (int i = 0; i < count; i++)
+                {
+                    res.Append(roman[index]);
+                }
+                num %= value[index];
+                index++;
+            }
+            return res.ToString();
         }
         #endregion
     }
